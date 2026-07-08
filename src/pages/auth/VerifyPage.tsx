cd3d9a14133fdf24b7bc329271fbad4a9568aee6
Ibrahim Herith <ibrahimherith19@gmail.com>
2026-07-08 12:00:07 +0300

    Updates on Auth and Landing Page

--- src/pages/auth/VerifyPage.tsx
+++ src/pages/auth/VerifyPage.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { ChevronLeftIcon } from "lucide-react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import { REGEXP_ONLY_DIGITS } from "input-otp";
 
